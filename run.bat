f85592c6b0397c69e44d8d6b1a92e171bd0edae9
Manish Jain <manish.jain1610@gmail.com>
2025-01-27 20:42:52 +0530

    Adding more automated pages.

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
+REM Change the directory to your local directory where project is installed. I have given example of my local directory.
+
 cd D:\Manish\IntelliJProjects\tests\The-Heroku-App-Selenium
-mvn clean install
+mvn clean test
